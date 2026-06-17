938542f415c2ac409ea5be619adb6432fb7d82ef
flykyung <sspsk1202@gmail.com>
2026-06-17 10:12:28 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,7 @@
+var GAS_URL = 'https://script.google.com/macros/s/AKfycbyb5RCoCMF6v7lPP7KmIez5DUpFaN0c9AS_E5MYEMmGhEbJD540Jc3RgoLXvyXBVU1yVQ/exec';
 
+var MODES = {
+  earn:  { banner: '퀴즈 적립 모드입니다.',            subtitle: '적립 모드',      bannerClass: 'earn'  },
+  spend: { banner: '부스 선택 후 QR을 스캔하세요.',    subtitle: '사용 모드',      bannerClass: 'spend' },
+  check: { banner: 'QR을 스캔하면 잔액을 확인합니다.', subtitle: '잔액 확인 모드', bannerClass: 'check' }
+};
